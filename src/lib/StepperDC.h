c0cc19f498d3a9229dc367c17b1ac6f2a649c536
Pawel Kusmierski <pawel.kusmierski@gmail.com>
2021-03-22 01:23:53 +0100

    trying to figure it out

--- src/lib/StepperDC.h
+++ src/lib/StepperDC.h
@@ -158,7 +158,8 @@ class stepperDC {
     }
 
     byte _phase=1;
-    bool _A4988=false;
+    bool _A4988=true;
+//    bool _A4988=false;
     int _power=0;
     bool _en=false;
     int _stepPin=-1;
