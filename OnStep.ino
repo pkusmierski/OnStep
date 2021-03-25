67da5e39e14aa45a9ffde7f29193fd73ccc94d00
Pawel Kusmierski <pawel.kusmierski@gmail.com>
2021-03-25 01:15:09 +0100

    ifdef fixup

--- OnStep.ino
+++ OnStep.ino
@@ -372,8 +372,8 @@ void setup() {
     tmcAxis4.setup(AXIS4_DRIVER_INTPOL,AXIS4_DRIVER_DECAY_MODE,AXIS4_DRIVER_CODE,axis4Settings.IRUN,axis4SettingsEx.IHOLD);
   #endif
 
-  #if AXIS4_DRIVER_DC_MODE != OFF
-  //  foc1.powerDownActive(AXIS4_DRIVER_POWER_DOWN == ON, AXIS4_DRIVER_POWER_DOWN == STARTUP);
+  #if AXIS4_DRIVER_DC_MODE == OFF
+    foc1.powerDownActive(AXIS4_DRIVER_POWER_DOWN == ON, AXIS4_DRIVER_POWER_DOWN == STARTUP);
   #endif
 #endif
 
